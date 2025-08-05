603d66e54e827fbfb6c85ba6bf5c9cf267643d3a
Tiago <tiagosantos0412@gmail.com>
2025-08-05 13:39:31 -0300

    Implementando a classe subscribe com as informações do usuário.

---
+++ cypress/pages/subscribe.js
@@ -0,0 +1,27 @@
+class Subscribe {
+    selectorsList(){
+        const selectors = {
+            firstName: "[name='firstName']",
+            lastName: "[name='lastName']",
+            userName: "[name='userName']",
+            password: "[name='password']",
+            confirmPassword: "[name='confirmPassword']",
+            submitForm: '.SignUpForm-form',
+            bankName: "[name='bankName']",
+            routingNumber: "[name='routingNumber']",
+            accountNumber: "[name='accountNumber']",
+            saveBankAccount: '.BankAccountForm-submit',
+        }
+        return selectors
+    }
+
+    subscribeUser(firstName, lastName, userName, password){
+        cy.get(this.selectorsList().firstName).clear().type(firstName)
+        cy.get(this.selectorsList().lastName).clear().type(lastName)
+        cy.get(this.selectorsList().userName).clear().type(userName)
+        cy.get(this.selectorsList().password).clear().type(password)
+        cy.get(this.selectorsList().confirmPassword).clear().type(password)
+        cy.get(this.selectorsList().submitForm).click()
+    }
+}
+export default Subscribe
