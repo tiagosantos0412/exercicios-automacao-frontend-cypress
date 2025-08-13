e57505f04e88069fba842b1e9dda71c58b939cde
Tiago <tiagosantos0412@gmail.com>
2025-08-13 16:47:17 -0300

    Iniciando a spec de transactions.

--- cypress/e2e/transactions.cy.js
+++ cypress/e2e/transactions.cy.js
@@ -1,1 +1,25 @@
 import userData from '../fixtures/users/user-data.json'
+import Login from '../pages/login'
+import Transactions from '../pages/transactions'
+
+const login = new Login()
+const transaction = new Transactions()
+//Load Chance
+const Chance = require('chance')
+//Instanciate Chance
+var chance = new Chance()
+
+const infoTransaction = {
+    amount: chance.floating({min: 0, max: 100, fixed: 2}), 
+    description: chance.word()
+}
+
+describe('realworld-app Tests',()=>{
+  it('New Transaction Payment', ()=>{
+    login.loginWithAnyUser(
+       userData.transactionUser.userName,
+       userData.transactionUser.password 
+    )
+    
+  })
+})
